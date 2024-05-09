eb71bc6905303889175c0cab702b8e0afc567c94
swombat <daniel.github@tenner.org>
2024-05-09 15:05:16 +0100

    Start by making sure this can run alongside another app that uses Sidekiq - switch to database 10.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,3 +7,25 @@ if heroku?
     config.redis = {ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}}
   end
 end
+
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: "redis://localhost:6379/10" }
+  end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: "redis://localhost:6379/10" }
+  end
+end
+
+if Rails.env.development?
+  Sidekiq.configure_server do |config|
+    config.logger = Logger.new(Rails.root.join("log/sidekiq_development.log"))
+    config.logger.level = Logger::DEBUG
+  end
+elsif Rails.env.test?
+  Sidekiq.configure_server do |config|
+    config.logger = Logger.new(Rails.root.join("log/sidekiq_test.log"))
+    config.logger.level = Logger::DEBUG
+  end
+end
