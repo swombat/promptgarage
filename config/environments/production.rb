a2f1ff585b369373085df901a355795df35bb702
swombat <daniel.github@tenner.org>
2024-05-14 11:57:30 +0100

    Aaand renamed to promptgarage to avoid conflict with... Microsoft.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "promptbench_production"
+  # config.active_job.queue_name_prefix = "promptgarage_production"
 
   config.action_mailer.perform_caching = false
 
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: "_promptbench_session"
+Rails.application.config.session_store :cookie_store, key: "_promptgarage_session"
